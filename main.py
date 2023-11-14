8d5f46ce9294543cb1db2717ef4dc06398ebfa7e
Isha Chaudhari <138683346+Isha-Chaudhari@users.noreply.github.com>
2023-11-14 16:19:17 +0530

    main.py

--- main.py
+++ main.py
@@ -1,90 +1,102 @@
-from flask import Flask, jsonify, request
-import cv2
-import numpy as np
-import scipy.signal as sig
-import cloudinary
-import cloudinary.uploader
-import cloudinary.config
-import requests
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-app = Flask(__name__)
+class VideoUploadApp extends StatefulWidget {
+  @override
+  _VideoUploadAppState createState() => _VideoUploadAppState();
+}
 
-# Configure Cloudinary settings
-cloudinary.config(
-    cloud_name="dggwc2zze",
-    api_key="382656316639283",
-    api_secret="jWnbVvXkRSxzgWFfdZbSshOt6kQ"
-)
+class _VideoUploadAppState extends State<VideoUploadApp> {
+  File? _videoFile;
+  String? _videoUrl;
 
-def fetch_video_from_cloudinary(video_url):
-    try:
-        response = cloudinary.uploader.download(video_url)
-        video_file = response["url"]
-        cap = cv2.VideoCapture(video_file)
-        return cap
-    except Exception as e:
-        return None
+  Future<void> _uploadVideo(ImageSource source) async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? pickedFile = await picker.pickVideo(source: source);
+    setState(() {
+      if (pickedFile != null) _videoFile = File(pickedFile.path);
+    });
 
-def calculate_blood_pressure(ppg_data):
-    try:
-        fs = 100   # Sampling rate in Hz
-        lowpass_fc = 8   # Low-pass filter cutoff frequency in Hz
-        ppg_data = sig.detrend(ppg_data)
-        b, a = sig.butter(4, lowpass_fc / (fs / 2), 'low')
-        ppg_data = sig.filtfilt(b, a, ppg_data)
-        systolic_peak_idx = np.argmax(ppg_data)
-        diastolic_notch_idx = np.argmin(ppg_data[systolic_peak_idx:])
-        diastolic_notch_idx += systolic_peak_idx
-        time_delay = (diastolic_notch_idx - systolic_peak_idx) / fs
-        arterial_distance = 0.7  # Distance in meters (sample value)
-        pwv = arterial_distance / time_delay
-        systolic_pressure = 9.6 * pwv + 120
-        diastolic_pressure = 2.0 * pwv + 80
-        return systolic_pressure, diastolic_pressure
-    except Exception as e:
-        return None, None
+    if (pickedFile != null) {
+      final url = Uri.parse('https://api.cloudinary.com/v1_1/dggwc2zze/upload');
+      final request = http.MultipartRequest('POST', url)
+        ..fields['upload_preset'] = 'vjr2zecn'
+        ..files
+            .add(await http.MultipartFile.fromPath('file', _videoFile!.path));
+      final Response = await request.send();
+      if (Response.statusCode == 200) {
+        final responseData = await Response.stream.toBytes();
+        final responseString = String.fromCharCodes(responseData);
+        final jsonMap = jsonDecode(responseString);
+        setState(() {
+          final url = jsonMap['url'];
+          _videoUrl = url;
+        });
+      }
+    }
+  }
 
-@app.route("/", methods=['POST'])
-def process_video():
-    try:
-        data = request.json()
-        video_url = data.get('video_url')
+  Future<void> _sendVideoUrlToBackend() async {
+    if (_videoUrl != null) {
+      var url = 'https://bp-ow6374e7c-isha-cs-projects.vercel.app';
+      var response = await http.post(
+        Uri.parse(url),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'video_url': _videoUrl ?? '',
+        }),
+      );
 
-        if not video_url:
-            return jsonify({"error": "No video URL provided"})
+      if (response.statusCode == 200) {
+        print('Video URL sent to the backend');
+        print("Response: ${response.body}");
+      } else {
+        print('Failed to send video URL to the backend');
+        print(response.statusCode);
+      }
+    }
+  }
 
-        cap = fetch_video_from_cloudinary(video_url)
-
-        if cap is None:
-            return jsonify({"error": "Failed to fetch the video from Cloudinary"})
-
-        print_count = 0
-        ppg_data = []
-
-        while print_count < 500:
-                ret, frame = cap.read()
-                if not ret:
-                    break
-
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                mean_brightness = cv2.mean(gray)[0]
-                ppg_data.append(mean_brightness)
-
-                print_count += 1
-
-        cap.release()
-        systolic_pressure, diastolic_pressure = calculate_blood_pressure(ppg_data)
-
-        if systolic_pressure is not None and diastolic_pressure is not None:
-            response = {
-                "Estimated systolic pressure": systolic_pressure,
-                "Estimated diastolic pressure": diastolic_pressure
-            }
-            return jsonify(response)
-        else:
-            return jsonify({"error": "Blood pressure estimation failed"})
-    except Exception as e:
-        return jsonify({"error": "An error occurred during video processing"})
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video Upload App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                _uploadVideo(ImageSource.camera);
+              },
+              child: Text('Capture Video from Camera'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _uploadVideo(ImageSource.gallery);
+              },
+              child: Text('Select Video from Gallery'),
+            ),
+            SizedBox(height: 20),
+            Text('Video URL: ${_videoUrl ?? "Not available"}'),
+            ElevatedButton(
+              onPressed: () {
+                _sendVideoUrlToBackend();
+              },
+              child: Text('Send Video URL to Backend'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
